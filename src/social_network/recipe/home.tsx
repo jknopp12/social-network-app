327a859ebbd3f3d2bdc174572f5c8d5ffa51d0ea
jknopp12 <129903306+jknopp12@users.noreply.github.com>
2024-04-20 20:49:31 -0400

    recipe home edits

--- src/social_network/recipe/home.tsx
+++ src/social_network/recipe/home.tsx
@@ -39,13 +39,13 @@ export default function RecipeHome() {
                 </p>
                 <div className="row mt-4">
                     {recipes.slice(-8).map((recipe) => (
-                        <div key={recipe._id} className="col-md-3 col-6 mb-4">
+                        <div key={recipe._id} className="col-md-4 col-lg-3 col-6 mb-4">
                             <div className="card">
                                 <Link to={`/Recipes/${recipe._id}`} style={{ textDecoration: 'none' }}>
                                     <div className="card-body">
-                                        <h5 className="card-title">{recipe.name}</h5>
-                                        <p className="card-text">{recipe.description}</p>
-                                        <CiBowlNoodles className="card-icon text-right position-absolute bottom-0 end-0" />
+                                        <h5 className="recipe-card-title">{recipe.name}</h5>
+                                        <p className="recipe-card-txt">{recipe.description}</p>
+                                        <CiBowlNoodles className="card-icon-t text-right position-absolute bottom-0 end-0" />
                                     </div>
                                 </Link>
                             </div>
