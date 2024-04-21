5ca655bed29abd05c98d5b07bc0546248000a0ea
jknopp12 <129903306+jknopp12@users.noreply.github.com>
2024-04-21 15:37:27 -0400

    post entries are wide now

--- src/social_network/profile/post.tsx
+++ src/social_network/profile/post.tsx
@@ -55,13 +55,13 @@ export default function NewPost() {
                     <div>
                         <div className="mb-3">
                             <label htmlFor="name" className="form-label">Recipe Name</label>
-                            <input className="input form-control wide-input" placeholder="Name" value={recipe.name} onChange={(e) =>
+                            <input className="form-control wide-input" placeholder="Name" value={recipe.name} onChange={(e) =>
                                 setRecipe({ ...recipe, name: e.target.value })} />
                         </div>
                         <div className="mb-3">
                             <label htmlFor="description" className="form-label"> Recipe Description</label>
                             <textarea
-                                className="input form-control wide-input"
+                                className="form-control wide-input"
                                 placeholder="Description"
                                 value={recipe.description}
                                 onChange={(e) => setRecipe({ ...recipe, description: e.target.value })}
@@ -70,7 +70,7 @@ export default function NewPost() {
                         <div className="mb-3">
                             <label htmlFor="ingredients" className="form-label">Recipe Ingredients</label>
                             <textarea
-                                className="input form-control wide-input"
+                                className="form-control wide-input"
                                 placeholder="Ingredients"
                                 value={recipe.ingredients}
                                 onChange={(e) => setRecipe({ ...recipe, ingredients: e.target.value.split(',') })}
@@ -79,7 +79,7 @@ export default function NewPost() {
                         <div className="mb-3">
                             <label htmlFor="instructions" className="form-label ">Recipe Instructions</label>
                             <textarea
-                                className="input form-control wide-input"
+                                className="form-control wide-input"
                                 placeholder="Instructions"
                                 value={recipe.instructions}
                                 onChange={(e) => setRecipe({ ...recipe, instructions: e.target.value.split('.') })}
